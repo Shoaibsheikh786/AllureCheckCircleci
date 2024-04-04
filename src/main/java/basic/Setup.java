136a98c5e953481c454a36af2fd39ef32e5be4ac
Muneer Ahmad <shoaib786asme@gmail.com>
2024-04-04 22:58:03 +0530

    changed chromedriver

--- src/main/java/basic/Setup.java
+++ src/main/java/basic/Setup.java
@@ -2,6 +2,7 @@ package basic;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Setup {
 	
@@ -9,7 +10,11 @@ public class Setup {
 	
 	public static void init()
 	{
-		driver=new ChromeDriver();
+		ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless"); // Run Chrome in headless mode
+        options.addArguments("--no-sandbox"); // Bypass OS security model
+        options.addArguments("--disable-dev-shm-usage"); // Fix session not created: This version of ChromeDriver only supports Chrome version XX error
+        driver = new ChromeDriver(options);
 	}
 
 }
