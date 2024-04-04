dfebc332e5b5d6f01c9820cb568221e68c97569b
Muneer Ahmad <shoaib786asme@gmail.com>
2024-04-04 23:14:52 +0530

    push code

--- src/main/java/basic/Setup.java
+++ src/main/java/basic/Setup.java
@@ -1,8 +1,11 @@
 package basic;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class Setup {
 	
@@ -14,7 +17,13 @@ public class Setup {
         options.addArguments("--headless"); // Run Chrome in headless mode
         options.addArguments("--no-sandbox"); // Bypass OS security model
         options.addArguments("--disable-dev-shm-usage"); // Fix session not created: This version of ChromeDriver only supports Chrome version XX error
-        driver = new ChromeDriver(options);
+     
+        try {
+			driver = new RemoteWebDriver(new URL("172.25.0.3"),options);
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
